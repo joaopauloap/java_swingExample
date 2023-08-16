d45e79d0577c9f9a67f8a5deab32f1226a1f200a
João Paulo de Araújo Porto <joaopaulo.porto@foursys.com.br>
2023-08-16 15:33:13 -0300

    update

--- src/swingExample/SwingExample1.java
+++ src/swingExample/SwingExample1.java
@@ -5,9 +5,16 @@ import javax.swing.*;
 
 public class SwingExample1 {
 	public static void main(String[] args) {
-		JFrame f = new JFrame("Button Example");
-		final JTextField tf = new JTextField();
+		JFrame f = new JFrame("Swing Example 1");
+		
+		JLabel l = new JLabel("This is a label");
+		l.setBounds(50,30,150,20);
+		f.add(l);
+		
+		JTextField tf = new JTextField();
 		tf.setBounds(50, 50, 150, 20);
+		f.add(tf);
+		
 		JButton b = new JButton("Click Here");
 		b.setBounds(50, 100, 95, 30);
 		b.addActionListener(new ActionListener() {
@@ -16,7 +23,7 @@ public class SwingExample1 {
 			}
 		});
 		f.add(b);
-		f.add(tf);
+
 		f.setSize(400, 400);
 		f.setLayout(null);
 		f.setVisible(true);
